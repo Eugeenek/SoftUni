31cc4acb3ca663b2936c2f403cda03b1ed7388a6
Eugeenek <eugeenk@gmail.com>
2015-05-13 20:05:05 +0300

    Homework Commit

---
+++ AdvancedC#/HomeworkArraysListsStacksQueues/Problem 2-SortArrayOfNumbersUsingSelectionSort/SelectionSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class SelectionSort
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter your numbers");
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int temp = 0;
+            int min = 0;
+
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                min = i;
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[j]<numbers[min])
+                    {
+                        min = j;
+                    }
+                }
+                temp = numbers[min];
+                numbers[min] = numbers[i];
+                numbers[i] = temp;
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i]);
+            }
+        }
+    }
+
