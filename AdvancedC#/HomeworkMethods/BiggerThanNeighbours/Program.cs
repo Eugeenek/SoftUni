7386c2f10ac498bc2cd7c01fb767992ec3590bf0
Eugeenek <eugeenk@gmail.com>
2015-05-15 23:04:21 +0300

    COmmit

---
+++ AdvancedC#/HomeworkMethods/BiggerThanNeighbours/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+
+    
+    static bool IsBiggerThanNeighbours(int[] array, int i)
+    {
+        bool isGreater = false;
+       
+            if (i==0)
+            {
+                isGreater = array[i] > array[i + 1];
+            }
+            else if ( i== array.Length - 1)
+            {
+                isGreater = array[i] > array[i-1];
+            }
+            else
+            {
+                isGreater = array[i] > array[i - 1] && array[i] > array[i + 1];
+            }
+            return isGreater;
+      
+    }
+
+    static void Main(string[] args)
+    {
+       
+        int [] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(IsBiggerThanNeighbours(array,i));
+            
+        }
+    }
+}
+
---
+++ AdvancedC#/MultidimensionalArrays/MaximalSum/MaximalSum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+class MaximalSum
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(new char[] {' '},StringSplitOptions.RemoveEmptyEntries);
+ 
+        int rows = int.Parse(input[0]) + 1;
+        int cols = int.Parse(input[1]) + 1;
+        int[,] matrix = new int[rows, cols];
+        for (int row = 0; row < rows-1; row++)
+        {
+            string[] currentRowNumbersAsStrings =
+                        Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            for (int col = 0; col < cols-1; col++)
+            {
+                matrix[row, col] = int.Parse(currentRowNumbersAsStrings[col]);
+            }
+        }
+ 
+        int bestSum = int.MinValue;
+        int currentSum = 0;
+        int[,] maximalPlatform = new int[3, 3];
+        for (int row = 0; row < rows - 2; row++)
+        {
+            for (int col = 0; col < cols - 2; col++)
+            {
+                currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] +
+                             matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
+                             matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+                if (currentSum > bestSum)
+                {
+                    bestSum = currentSum;
+                    for (int i = 0; i < maximalPlatform.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < maximalPlatform.GetLength(1); j++)
+                        {
+                            maximalPlatform[i, j] = matrix[row + i, col + j];
+                        }
+                    }
+                }
+            }
+        }
+ 
+        Console.WriteLine("Sum = {0}", bestSum);
+        for (int row = 0; row < maximalPlatform.GetLength(0); row++)
+        {
+            for (int col = 0; col < maximalPlatform.GetLength(1); col++)
+            {
+                Console.Write("{0,2} ", maximalPlatform[row, col]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
