73c631def27ac7016787e1bc515f7812007bc64b
Eugeenek <eugeenk@gmail.com>
2015-06-08 15:44:54 +0300

    ExceptionHAndling Homework

---
+++ AdvancedC#/ExceptionHandling/CalcualateSqrt/CalculateSqrt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcualateSqrt
+{
+    class CalculateSqrt
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                int number = int.Parse(input);
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                double result = Math.Sqrt(number);
+                Console.WriteLine("Sqrt of the number you have entered is {0}", result);
+            }
+            catch(ArgumentOutOfRangeException)  
+            {
+                Console.Error.WriteLine("You should enter a positive number!");
+            }            
+            catch (FormatException)
+            {
+
+                Console.WriteLine("You need to enter valid numeric value!");
+            }
+            catch (OverflowException)
+            {
+                Console.Error.WriteLine("The number you have entered is outside of the range of Int32");
+            }
+            finally
+            {
+                Console.WriteLine("Good Bye!");
+            }
+            
+        }
+    }
+}
---
+++ AdvancedC#/ExceptionHandling/EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterNumbers
+{
+    class EnterNumbers
+    {
+        static void Main()
+        {
+            int count = 1;
+            int start = 1;
+            string input = Console.ReadLine();
+
+            while (count<=10)
+            {
+                try
+                {
+                    int number = int.Parse(input);
+                    ReadNumbers(start, number);
+                    count++;
+                    start = number;
+                }
+                catch (FormatException)
+                {
+
+                    Console.Error.WriteLine("You should enter numeric value!");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.Error.WriteLine("The numbers you are entering should be in range [{0}...100]", start);
+                }
+                finally
+                {
+                    Console.WriteLine("Enter new number in range [{0}...100]", start);
+                    input = Console.ReadLine();
+                }
+            }
+        }
+
+        private static void ReadNumbers(int start, int n)
+        {
+            if (n<=start||n>=100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
